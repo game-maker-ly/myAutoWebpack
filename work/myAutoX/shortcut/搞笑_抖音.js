72c72ffec759a62e45be71e1293de1300f8e0bad
rua neko <rua_neko@163.com>
2024-06-08 22:26:26 +0800

    更改图标

--- work/myAutoX/shortcut/搞笑_抖音.js
+++ work/myAutoX/shortcut/搞笑_抖音.js
@@ -2,4 +2,5 @@ var dyApp = require("../app/打开抖音.js");
 
 dyApp.searchWithType("不干人事", "video");
 
+sleep(2000);
 click(300, 500);
--- work/myAutoX/shortcut/美食_抖音.js
+++ work/myAutoX/shortcut/美食_抖音.js
@@ -2,4 +2,5 @@ var dyApp = require("../app/打开抖音.js");
 
 dyApp.searchWithType("一起来做菜", "video");
 
+sleep(2000);
 click(300, 500);
