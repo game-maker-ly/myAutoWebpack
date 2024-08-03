b2a1acf13343f3e8b6b7eff234b900c13fc70d48
rua neko <rua_neko@163.com>
2024-08-03 20:35:05 +0800

    修复bug

--- work/myPhone/tasker/任务_亮屏触发.js
+++ work/myPhone/tasker/任务_亮屏触发.js
@@ -6,12 +6,13 @@ const lockTool = require("../lib/模块_锁.js");
 
 // 比锁有bug
 // 如果锁住
-if (lockTool.getLocked()) {
+// 不修了,有几率永久锁住
+if (lockTool.getLocked("screen_on")) {
     // 就停止执行
     exit();
 } else {
     // 设置锁
-    lockTool.setLocked(true);
+    lockTool.setLocked(true, "screen_on");
 }
 // 触发按键监听，回调传入手电筒执行函数
 // 清理其他脚本，这里只需要保证亮屏清理手电筒即可
@@ -20,7 +21,7 @@ log("亮屏触发");
 // 重复触发了，得防止重复
 
 // 执行完毕，释放锁
-lockTool.setLocked(false);
+lockTool.setLocked(false, "screen_on");
 
 // 简单来说就是：息屏-》返回桌面，并停止其他脚本，开启一个按键监听任务（监听音量键按下则执行手电筒的快捷方式
 // 亮屏则停止手电筒的监听，然后开启来电状态监听，如果接电话则修改为外放，如果挂电话就还原
