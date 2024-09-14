6bf2409b2bb8c35780539aff7446ae4ac1c59c35
rua neko <rua_neko@163.com>
2024-09-14 19:00:22 +0800

    添加芒果tv记忆进度，以及选集跳过

--- work/myAutoX/app/打开芒果tv_国际版_电视版.js
+++ work/myAutoX/app/打开芒果tv_国际版_电视版.js
@@ -1,7 +1,69 @@
 const myFloaty = require("../lib/模块_悬浮窗扩展.js");
 
+// 还需要注册一个方法，来缓存进度
+// 当脚本开始时获取当前时间
+// 脚本退出时获取时间，得到脚本打开时长（可能需要校正
+// 然后将打开时长与缓存中的时长累加，得到总的观看时长并缓存
+// 下次打开就直接用playTime（用的是毫秒数）定位即可，需要写在app方法里
+// 因为这个版本没办法记忆进度，不如手机版操作方便
+// 需要对不同的videoId分开记忆进度
+// 那只能放在openVideoId方法里面
+// 但又有个问题，退出/换集都需要保存时间
+// 那也就是要区分第一次打开和后续打开
+
+// 是否需要重置播放进度
+// 根据上次打开时间，还是总的观看时长？过滤videoId
+
+
+var isFirst = true;
+var lastVideoId;
+var lastTime;
+var play_time = 0;
+const st = storages.create("MGTV.Cache.PlayTime");
 // 芒果tv电视版，懒得转屏幕和找全屏控件了，直接用横屏的
 function _openVideoById(videoId) {
+    // 区分第一次调用
+    if (isFirst) {
+        isFirst = false;
+        // 根据videoId记忆进度
+        // 脚本退出时缓存观看时长（总
+        // 仅注册1次，lastVideoId在这段代码之前是上一次videoId
+        // 在这段代码之后就变为当前打开的videoId
+        events.on("exit", function () {
+            // 缩短播放时长，防止跳帧
+            var curPlayTime = new Date() - lastTime - 3900;
+            // 累加观看时长
+            play_time += curPlayTime;
+            log("记忆播放进度：" + play_time);
+            st.put(lastVideoId, { totalTime: play_time, lastPlayTime: curPlayTime });
+        });
+    } else {
+        // 换集记忆，记忆上次打开的videoId
+        var curPlayTime = new Date() - lastTime - 3900;
+        // 累加观看时长
+        play_time += curPlayTime;
+        log("记忆播放进度：" + play_time);
+        st.put(lastVideoId, { totalTime: play_time, lastPlayTime: curPlayTime });
+        log(lastVideoId);
+        // 然后还需要设置这一集的进度
+    }
+    // 初次打开还是换集，从缓存中取出playTime都是共用的
+    var pt_obj = st.get(videoId);
+    log(pt_obj);
+    if (!pt_obj) {
+        // 如果缓存中没有，则视为0
+        // 上次打开时长为极大值，用于之后过滤
+        pt_obj = { totalTime: 0, lastPlayTime: 99999 };
+    }
+    /// 从缓存中取出总的观看时长，用于之后打开app
+    play_time = pt_obj["totalTime"];
+    log(play_time);
+    // 暂存此次的id，用于下一次打开
+    lastVideoId = videoId;
+    // 暂存此次打开时间，用于保存时间差
+    lastTime = new Date();
+
+
     // 需要销毁活动，否则partId不生效，或者结合clipId也是一样的效果
     // 这里partId就相当于videoId，还有个PlId，播放列表id
     // 国内版本可以使用fullScreen=1&fullPlay=1来达到全屏
@@ -10,6 +72,11 @@ function _openVideoById(videoId) {
     // 国际版唯一的好处就是无更新和广告
     // 不过缺点就是片源少，另外部分scheme不生效
     var videoUrl = util.format("mgtvintl://vod/player?partId=%s&fullScreen=1&fullPlay=1", videoId);
+    // 如果观看时长不为0，说明已有播放记录，则拼接playTime参数（单位毫秒
+    if (play_time) {
+        videoUrl += "&playTime=" + play_time;
+        log(videoUrl);
+    }
     app.startActivity({
         action: "VIEW",
         flags: ["ACTIVITY_CLEAR_TOP", "ACTIVITY_CLEAR_TASK", "ACTIVITY_NEW_TASK"],   //清除活动，返回主页，对芒果TV无效
--- work/myAutoX/net/芒果tv网络请求.js
+++ work/myAutoX/net/芒果tv网络请求.js
@@ -5,6 +5,12 @@
 // 在各自的业务层去反转数组
 // cid是必须的，集合id，一般也代表某一年
 // videoid和month选一个
+// 是否需要缓存同样url的结果？
+var lastUrl;
+var lastRejson;
+
+var isIgnoreCache = false;
+const st = storages.create("MGTV.Cache.PlayTime");
 function _getShowList(isNewest, collection_id, video_id, month) {
     var url;
     if (isNewest) {
@@ -23,27 +29,63 @@ function _getShowList(isNewest, collection_id, video_id, month) {
     if (month) {
         url += "&month=" + month;
     }
-    // 请求
-    var res = http.get(url);
-    if (res.statusCode == 200) {
-        var rejson = res.body.json();
-        // 这里需要把节目列表反转，保持第0个为最新
-        if (!isNewest) {
-            rejson.data.list.reverse();
-        }
-        return rejson;
+    var rejson;
+    // 重新请求
+    if (lastUrl != url) {
+        lastUrl = url;
+        var res = http.get(url);
+        // 如果请求失败返回false
+        if (res.statusCode != 200) return false;
+        rejson = res.body.json();
+        // 深拷贝
+        lastRejson = JSON.parse(JSON.stringify(rejson));
+    } else {
+        // 调用缓存
+        rejson = JSON.parse(JSON.stringify(lastRejson));
     }
-    return false;
+    // 这里需要把节目列表反转，保持第0个为最新
+    if (!isNewest) {
+        rejson.data.list.reverse();
+    }
+    // isIgnoreCache为true，那么就过滤缓存中的videoId
+    // 还是用局部变量吧，主要是耦合太高了
+    // 用递归？跳过挺难搞的，因为会导致一个问题，
+    // list_tmp可能为空，这样会报错
+    // 当list_tmp为空，需要清理当前月？
+    // 还是用递归？
+    // 简单来说，可能会存在从2024最新一集，直接跳到2022第一集的极端情况
+    // 最好还是模拟一步步跳吧
+    // log(rejson.data.list);
+    if (isIgnoreCache) {
+        var list_tmp = [];
+        for (let i = 0; i < rejson.data.list.length; i++) {
+            var tmp = rejson.data.list[i];
+            var vid = tmp["video_id"];
+            if (st.contains(vid)) {
+                var lastPlayTime = st.get(vid)["lastPlayTime"];
+                // 当打开时间超过30s，则认为没有看过，否则过滤
+                if (lastPlayTime > 30000) {
+                    list_tmp.push(tmp);
+                }
+            } else {
+                // 缓存中没有则直接添加
+                list_tmp.push(tmp);
+            }
+        }
+        rejson.data.list = list_tmp;
+    }
+    // log("测试后：", rejson.data.list);
+    return rejson;
 }
 
 
 // 根据节目id获取最新的videoId
 function _getNewVideoId(collection_id) {
+    isIgnoreCache = false;
     // 不能传入vid
     var rejson = _getShowList(true, collection_id);
     if (rejson) {
         var newVideoInfo = rejson.data.list[0];
-        // log(newVideoInfo);
         var videoId = newVideoInfo.video_id;
         var videoName = newVideoInfo.t3;
         log(videoName);
@@ -65,19 +107,19 @@ function _getPageByMonth(month, collection_id, isNewest) {
 
 // 根据年份获取对应合集的页
 function _getPageByYearId(year_id, isNewest) {
-    var rejson = _getShowList(isNewest, year_id);
-    if (rejson) {
-        var stream_list = rejson.data.list;
-        return stream_list;
-    }
-    return -1;
+    return _getShowList(isNewest, year_id);
 }
 
 
 // 根据当前月份，和传入月份数组，获取到相邻月份
 // 也可以适用查找相邻videoId
 // 如果不存在则返回-1
+// 这里没有考虑tab_m为[]的情况，会报错
+// 或许要在这里使用递归？
 function _getNearMonth(paramName, month, tab_m, isPrev) {
+    // 反转
+    tab_m = JSON.parse(JSON.stringify(tab_m));
+    tab_m.reverse();
     var res_month = -1;
     for (let i = 0; i < tab_m.length; i++) {
         var m = tab_m[i][paramName];
@@ -121,8 +163,21 @@ function _getVideoNameById(paramName, res_paramName, video_id, stream_list) {
 // 过滤极端情况，比如index为0，或为total-1，那说明在第一或者最后
 // 该执行分页了
 // 分页又分为上和下，如果找不到需要的页，说明没找到video
+// 这里需要自定义进度缓存，
+// 以及过滤掉已有的videoId
+// 调用这个方法，说明当前的video_id是看过了的，
+// 那就需要缓存在storage，下次打开就跳过已缓存的
+// 那干脆写在_getShowList方法里，用布尔值控制是否过滤
+
+
+// 干脆在这里做个递归，当获取到不在缓存里面的则停止
 
 function _getNearVideoId(collection_id, video_id, isPrev) {
+    // log("触发_getNearVideoId方法---");
+    // 获取网络数据
+    // 问题在于要减少网络请求，来达到跳过重复值的目的
+    isIgnoreCache = true;
+    var cache_yid_list = [];
     var rejson = _getShowList(false, collection_id, video_id);
     if (rejson) {
         var nearVideoId = -1;
@@ -133,9 +188,6 @@ function _getNearVideoId(collection_id, video_id, isPrev) {
         var tab_y = rejson.data.tab_y;  // 年份页
         var cur_yid = collection_id;  // 当前年份，准确来说是当前集合id
         // 反转年份，月份和节目页，升序排序
-        stream_list.reverse();
-        tab_m.reverse();
-        tab_y.reverse();
         // 先尝试获取附近的videoId
         var nearVideoId_tmp = _getNearMonth("video_id", video_id, stream_list, isPrev);
         // 不需要分页的情况
@@ -144,40 +196,87 @@ function _getNearVideoId(collection_id, video_id, isPrev) {
             nearVideoName = _getVideoNameById("video_id", "t3", nearVideoId, stream_list);
         } else {
             // 没找到就需要分页找
-            var near_month = _getNearMonth("m", cur_month, tab_m, isPrev);
-            log("尝试查找附近月份");
-            // 找到相邻月份，根据月份获取分页
-            if (near_month != -1) {
-                // 找出对应月份对应的节目页
-                var stream_list_byMonth = _getPageByMonth(near_month, collection_id, isPrev);
-                // 分页的情况
-                var idx = !isPrev ? stream_list_byMonth.length - 1 : 0;
-                nearVideoId = stream_list_byMonth[idx]["video_id"];
-                nearVideoName = stream_list_byMonth[idx]["t3"];
-            } else {
-                // 没找到月份那就找年份
-                log("尝试查找附近年份");
-                var near_yid = _getNearMonth("id", cur_yid, tab_y, isPrev);
-                var isNewest = isPrev;
-                if (near_yid == -1) {
-                    if (isPrev) {
-                        // 假设是2022年，往上找不到
-                        // 那就定位到最后一个，需要找最新的一期
-                        near_yid = tab_y[tab_y.length - 1]["id"];
-                    } else {
-                        // 否则就定位回第0个
-                        // 需要找第一期
-                        near_yid = tab_y[0]["id"];
+            // 做个do_while循环？
+            // 有个问题，month和year的循环要分开
+            var isFound = false;
+            var isYearChange = false;
+            do {
+                // 月份页是会随着当前年份而改变的，所以需要更改
+                var near_month = cur_month;
+                if (isYearChange) {
+                    isYearChange = false;
+                } else {
+                    near_month = _getNearMonth("m", cur_month, tab_m, isPrev);
+                }
+                log("尝试查找附近月份：" + near_month);
+                // 找到相邻月份，根据月份获取分页
+                if (near_month != -1) {
+                    // 找出对应月份对应的节目页
+                    // getPageByMonth可能为[]，那也就说明该月份下节目已经看完
+                    var stream_list_byMonth = _getPageByMonth(near_month, cur_yid, isPrev);
+                    // 如果当前月份下都已看过，那么就continue循环
+                    if (stream_list_byMonth.length == 0) {
+                        // 月份翻页
+                        cur_month = near_month;
+                        continue;
                     }
-                } 
-                var stream_list_byYear = _getPageByYearId(near_yid, isNewest);
-                // 分页的情况
-                var idx = !isPrev ? stream_list_byYear.length - 1 : 0;
-                nearVideoId = stream_list_byYear[idx]["video_id"];
-                nearVideoName = stream_list_byYear[idx]["t3"];
-                // 暂存当前年份
-                curYearId = near_yid;
-            }
+                    // 分页的情况
+                    var idx = !isPrev ? stream_list_byMonth.length - 1 : 0;
+                    // log(idx, stream_list_byMonth);
+                    nearVideoId = stream_list_byMonth[idx]["video_id"];
+                    nearVideoName = stream_list_byMonth[idx]["t3"];
+                    // 找到则终止循环
+                    isFound = true;
+                } else {
+                    // 没找到月份那就找年份
+                    var near_yid = _getNearMonth("id", cur_yid, tab_y, isPrev);
+                    if (near_yid == -1) {
+                        if (!isPrev) {
+                            // 假设是2022年，往上找不到
+                            // 那就定位到最后一个，需要找最新的一期
+                            near_yid = tab_y[tab_y.length - 1]["id"];
+                        } else {
+                            // 否则就定位回第0个
+                            // 需要找第一期
+                            near_yid = tab_y[0]["id"];
+                        }
+                    }
+                    log("尝试查找附近年份：" + near_yid);
+                    // 这里也可能为[]
+                    var pageInfoByYear = _getPageByYearId(near_yid, isPrev);
+                    var stream_list_byYear = pageInfoByYear.data.list;
+                    // 这个代表这个年是空的？
+                    // 应该代表这一年最新月是空的，所以有问题，得交给月份去查
+                    if (stream_list_byYear.length == 0) {
+                        // 月份也要改
+                        tab_m = pageInfoByYear.data.tab_m;
+                        tab_m = JSON.parse(JSON.stringify(tab_m));
+                        cur_month = isPrev ? tab_m[0].m : tab_m[tab_m.length - 1].m;
+                        // 翻页重复了，
+                        // log(cur_month, tab_m);
+                        isYearChange = true;
+                        // 移动年份
+                        cache_yid_list.push(cur_yid);
+                        // 说明已经找完一圈，无法找到，结束循环
+                        if (cache_yid_list.indexOf(near_yid) != -1) {
+                            break;
+                        }
+                        cur_yid = near_yid;
+                        // 暂存当前年份
+                        curYearId = near_yid;
+                        // 当然也可能存在找一圈也没找到情况，需要避免
+                        continue;
+                    }
+                    // 分页的情况
+                    // 跨页查找
+                    var idx = !isPrev ? stream_list_byYear.length - 1 : 0;
+                    nearVideoId = stream_list_byYear[idx]["video_id"];
+                    nearVideoName = stream_list_byYear[idx]["t3"];
+                    isFound = true;
+                    // 暂存当前年份
+                    curYearId = near_yid;
+                }
+            } while (!isFound);
         }
 
         // 如果找到了
@@ -204,17 +303,15 @@ function _getNearVideoId(collection_id, video_id, isPrev) {
 // 那这得用一个变量保存
 var curYearId;
 
-// var n_vid = _getNearVideoId(407130, 15624511, true);
-// log(curYearId, n_vid);
 
 exports.getCurYearId = function (collection_id) {
-    return curYearId;   
+    return curYearId;
 }
 
 exports.getNewVideoId = function (collection_id) {
-    return _getNewVideoId(collection_id);   
+    return _getNewVideoId(collection_id);
 }
 
 exports.getNearVideoId = function (collection_id, video_id, isPrev) {
-    return _getNearVideoId(collection_id, video_id, isPrev);   
+    return _getNearVideoId(collection_id, video_id, isPrev);
 }
