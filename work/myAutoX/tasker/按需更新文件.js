967e8dba70c18b7a24b8b2cf66f5718ea53cea38
rua neko <rua_neko@163.com>
2025-05-19 10:16:27 +0800

    修复bug

--- work/myAutoX/tasker/按需更新文件.js
+++ work/myAutoX/tasker/按需更新文件.js
@@ -24,72 +24,86 @@ var dl_Files;
 var localMd5;
 var cloudMd5;
 var m_count = 0;
-if (!isMD5Exists) {
-    FileTool.downloadFile(dirPath);
-    dl_Files = FileTool.getAllFilePaths();
-} else {
-    // 如果md5文件存在，就和云端上的md5文件作对比
-    // 下载云端的md5
-    var cloudPath = "md5_cloud.json";
-    cloudMd5 = FileTool.downloadFileAndRead(dirPath, cloudPath);
-    localMd5 = FileTool.getMd5Json();
-    // 最好还是写个对比两个json的方法
-    // 如果是删除了文件，那么云端没有对应的key
-    // 那实际上还是会遍历两遍
-    var needUpdateFiles = [];
-    for (path in cloudMd5) {
-        var md5val = cloudMd5[path];
-        var md5val_old = FileTool.getMd5ByPath(path);
-        if (md5val != md5val_old) {
-            // 执行下载/更新
-            // 不过有一种情况，云端文件改名或者删除了的话
-            // 客户端没法做操作
-            // 此处说明需要新增或者修改
-            needUpdateFiles.push(path);
-            m_count++;
-        }
-    }
-    // 异步并发更新文件
-    dl_Files = needUpdateFiles;
-}
 
-// 如果可下载文件数为0，不会发送广播，那么会导致一直阻塞，需要做个if判断
-if (dl_Files.length == 0) {
-    // 直接更新布局
-    // 然后退出当前脚本
-    log("无可更新文件，直接开始更新桌面布局。。。");
-    engines.execScriptFile("tasker/更新桌面布局.js");
-    //强制退出
-    exit();
-} else {
-    FileTool.downloadFileList_Async(dl_Files, () => {
-        // log("文件列表下载完成！");
-        if (isMD5Exists) {
-            log("已更新：" + m_count + "个文件");
-            var count = 0;
-            for (l_path in localMd5) {
-                // 如果云端md5文件没有对应的key，说明要删除
-                if (!(l_path in cloudMd5)) {
-                    files.remove(l_path);
-                    count++;
-                }
+// 注册销毁，释放引擎
+events.on("exit", function () {
+    log("按需更新文件已exit");
+});
+
+var t_lock_flag = false;
+var t_lock = threads.start(function () {
+    t_lock_flag = true;
+    // 异步代码开始
+    if (!isMD5Exists) {
+        FileTool.downloadFile(dirPath);
+        dl_Files = FileTool.getAllFilePaths();
+    } else {
+        // 如果md5文件存在，就和云端上的md5文件作对比
+        // 下载云端的md5
+        var cloudPath = "md5_cloud.json";
+        cloudMd5 = FileTool.downloadFileAndRead(dirPath, cloudPath);
+        localMd5 = FileTool.getMd5Json();
+        // 最好还是写个对比两个json的方法
+        // 如果是删除了文件，那么云端没有对应的key
+        // 那实际上还是会遍历两遍
+        var needUpdateFiles = [];
+        for (path in cloudMd5) {
+            var md5val = cloudMd5[path];
+            var md5val_old = FileTool.getMd5ByPath(path);
+            if (md5val != md5val_old) {
+                // 执行下载/更新
+                // 不过有一种情况，云端文件改名或者删除了的话
+                // 客户端没法做操作
+                // 此处说明需要新增或者修改
+                needUpdateFiles.push(path);
+                m_count++;
             }
-            log("已删除：" + count + "个文件");
-            // 完成后把云端的md5覆盖到本地
-            // cloud文件删不删都不影响，没必要在其他地方加
-            // 只需要保证md5和config文件本身正确即可
-            files.remove(dirPath);
-            files.rename(cloudPath, dirPath);
         }
-        // 创建快捷方式
+        // 异步并发更新文件
+        dl_Files = needUpdateFiles;
+    }
+    // 如果可下载文件数为0，不会发送广播，那么会导致一直阻塞，需要做个if判断
+    if (dl_Files.length == 0) {
+        // 直接更新布局
+        // 然后退出当前脚本
+        log("无可更新文件，直接开始更新桌面布局。。。");
         engines.execScriptFile("tasker/更新桌面布局.js");
-        // 强制退出
+        //强制退出
         exit();
-    });
-}
+    } else {
+        FileTool.downloadFileList_Async(dl_Files, () => {
+            // log("文件列表下载完成！");
+            if (isMD5Exists) {
+                log("已更新：" + m_count + "个文件");
+                var count = 0;
+                for (l_path in localMd5) {
+                    // 如果云端md5文件没有对应的key，说明要删除
+                    if (!(l_path in cloudMd5)) {
+                        files.remove(l_path);
+                        count++;
+                    }
+                }
+                log("已删除：" + count + "个文件");
+                // 完成后把云端的md5覆盖到本地
+                // cloud文件删不删都不影响，没必要在其他地方加
+                // 只需要保证md5和config文件本身正确即可
+                files.remove(dirPath);
+                files.rename(cloudPath, dirPath);
+            }
+            // 创建快捷方式
+            engines.execScriptFile("tasker/更新桌面布局.js");
+            // 强制退出
+            t_lock_flag = false;    // 释放线程锁
+            exit();
+        });
+    }
+    // 异步代码结束
+});
+t_lock.join();// 子线程阻塞主线程
+
 
 // 由于前面的下载文件是异步操作，后面的setTimeOut不会等到下载完毕才执行
-
+// 这个脚本可能不会处理!!!
 // 做个超时处理，假设30s超时
 // 一般情况下10s内能下完所有文件
 setTimeout(() => {
@@ -99,4 +113,4 @@ setTimeout(() => {
     // 仅清除定时器无法退出
     // clearInterval(myInterval);
     exit();
-}, 15 * 1000);
+}, 30 * 1000);
